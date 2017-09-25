d8f901c818689f41c2c43cafef4b0d6e620452e7
Lyle <5846films@gmail.com>
2017-09-25 13:25:56 -0700

    fixed Main.js

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,12 +1,8 @@
 import React from "react";
-import Navbar from "./common/Navbar";
-import Footer from "./common/Footer";
 
 const Main = props => (
   <div>
-    <Navbar />
     {props.children}
-    <Footer />
   </div>
 );
 
